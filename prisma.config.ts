a87d0896704a619d2c6b5aad28cb989e79797e58
Fatima Noorulainn <fatimanoorulainn@gmail.com>
2026-07-28 06:56:25 +0500

    fix prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,6 @@
 ﻿import { defineConfig } from 'prisma/config'
 
 export default defineConfig({
-  earlyAccess: true,
   schema: 'prisma/schema.prisma',
   datasource: {
     url: 'postgresql://postgres.ufszcsszxqpxrwggkjyb:lAwL7wP2abvkr5k4@aws-1-ap-northeast-2.pooler.supabase.com:5432/postgres',
