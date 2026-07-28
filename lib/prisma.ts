978095a289957089cababc0493ff7340f89a3ef9
Fatima Noorulainn <fatimanoorulainn@gmail.com>
2026-07-28 06:50:50 +0500

    fix prisma client import

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,9 @@
-﻿import { PrismaClient } from '@prisma/client'
-import { PrismaPg } from '@prisma/adapter-pg'
+﻿import { PrismaPg } from '@prisma/adapter-pg'
+const { PrismaClient } = require('@prisma/client')
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! })
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+const globalForPrisma = globalThis as unknown as { prisma: any }
 
 export const prisma =
   globalForPrisma.prisma || new PrismaClient({ adapter })
